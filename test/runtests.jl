2e7d06801be38ba3a414e5d60c8a3d2e86c4e3b6
Jacob Quinn <quinn.jacobd@gmail.com>
2025-03-28 08:32:05 -0600

    Add test for init method

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ using Test, Aqua, LibAwsChecksums, LibAwsCommon, CRC32c
     end
     @testset "basic usage to test the library loads" begin
         alloc = aws_default_allocator() # important! this shouldn't need to be qualified! if we generate a definition for it in LibAwsChecksums that is a bug.
+        LibAwsChecksums.init(alloc)
         input = rand(UInt8, 10)
         actual = aws_checksums_crc32c(input, 10, 0)
         expected = crc32c(input)
