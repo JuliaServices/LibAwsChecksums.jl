6557ae0206740e74803286e5c2ee210bafed80b1
Octogonapus <me@irrational.engineering>
2025-06-12 22:05:35 -0400

    run with debug info

--- gen/generate.sh
+++ gen/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir=$(dirname "$0")
-julia --project="$dir" -e 'using Pkg; Pkg.instantiate()'
-julia --project="$dir" -t auto "$dir/generator.jl"
+julia -g -O0 --project="$dir" -e 'using Pkg; Pkg.instantiate()'
+julia -g -O0 --project="$dir" -t auto "$dir/generator.jl"
